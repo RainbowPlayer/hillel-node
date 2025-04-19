9dc18eb4b56f02d4781bc03e9af59b1dbb444937
RainbowPlayer <kolak375@gmail.com>
2025-04-19 17:00:16 +0300

    add files

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const fs = require('fs');
+const { Transform } = require('stream');
 
-fs.copyFile('source.txt', 'copy.txt', (err) => {
-  if (err) {
-    console.error('Error copying file:', err);
-    process.exit(1);
+class MaskEveryLineTransform extends Transform {
+  constructor(options) {
+    super(options);
+    this._buffer = '';
   }
-  console.log('File successfully');
-});
+
+  _transform(chunk, encoding, callback) {
+    this._buffer += chunk.toString();
+    const lines = this._buffer.split(/\r?\n/);
+    this._buffer = lines.pop();
+
+    for (const line of lines) {
+      if (line.trim()) {
+        this.push('*'.repeat(line.length) + '\n');
+      } else {
+        this.push('\n');
+      }
+    }
+    callback();
+  }
+
+  _flush(callback) {
+    const line = this._buffer;
+    if (line && line.trim()) {
+      this.push('*'.repeat(line.length));
+    }
+    callback();
+  }
+}
+
+fs.createReadStream('log.txt', { encoding: 'utf8' })
+  .pipe(new MaskEveryLineTransform())
+  .pipe(fs.createWriteStream('masked.txt'))
+  .on('finish', () => console.log('success'))
