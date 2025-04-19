b43198519267fce4c65a31fa9f706409e8876410
RainbowPlayer <kolak375@gmail.com>
2025-04-19 15:18:05 +0300

    add files

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const fs = require('fs');
+const fs = require('fs/promises');
+const path = require('path');
 
-fs.copyFile('source.txt', 'copy.txt', (err) => {
-  if (err) {
-    console.error('Error copying file:', err);
+async function replaceWord() {
+  try {
+    const src = path.join(__dirname, 'source.txt');
+    const dest = path.join(__dirname, 'replaced.txt');
+
+    const text = await fs.readFile(src, 'utf-8');
+    const updated = text.replace(/Node/g, 'NODE.JS');
+
+    await fs.writeFile(dest, updated);
+
+    console.log('success');
+  } catch (err) {
+    console.error('error', err);
     process.exit(1);
   }
-  console.log('File successfully');
-});
+}
+
+replaceWord();
