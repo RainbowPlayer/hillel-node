97bd980ff904245d6b18c72717ee27074bf0dc30
RainbowPlayer <kolak375@gmail.com>
2025-04-19 16:37:41 +0300

    add files

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.copyFile('source.txt', 'copy.txt', (err) => {
-  if (err) {
-    console.error('Error copying file:', err);
-    process.exit(1);
-  }
-  console.log('File successfully');
+const stream = fs.createReadStream(
+  path.join(__dirname, 'log.txt'),
+  { encoding: 'utf8', highWaterMark: 100 }
+);
+
+let i = 0;
+stream.on('data', (chunk) => {
+  console.log(`--- Chunk #${++i} (length: ${chunk.length} chars) ---`);
+  console.log(chunk);
 });
+stream.on('end', () => console.log('--- End of file ---'));
+stream.on('error', (err) => console.error(err));
