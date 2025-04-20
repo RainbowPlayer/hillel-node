0603aa0d63cd1f7f844fe23139074c259bbf84d2
RainbowPlayer <kolak375@gmail.com>
2025-04-20 20:48:06 +0300

    add files

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const fs = require('fs');
 
-fs.copyFile('source.txt', 'copy.txt', (err) => {
+const fs = require('fs');const path = require('path');
+
+const filePath = path.join(__dirname, 'file.bin');
+
+fs.readFile(filePath, (err, buffer) => {
   if (err) {
-    console.error('Error copying file:', err);
+    console.error('Error reading file:', err);
     process.exit(1);
   }
-  console.log('File successfully');
+
+  console.log('Raw Buffer object:', buffer);
+
+  console.log('Hex dump:\n', buffer.toString('hex'));
+
+  console.log('Binary-as-text \n', buffer.toString('binary'));
 });
