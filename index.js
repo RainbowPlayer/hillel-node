5441b78cfcb957640dd0467005b583cbd1f42dc0
RainbowPlayer <kolak375@gmail.com>
2025-04-20 21:47:16 +0300

    add files

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const fs = require('fs');
+const cluster = require('cluster');
+const http = require('http');
+const os = require('os');
 
-fs.copyFile('source.txt', 'copy.txt', (err) => {
-  if (err) {
-    console.error('Error copying file:', err);
-    process.exit(1);
+const numCPUs = os.cpus().length;
+const PORT = process.env.PORT || 3000;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-  console.log('File successfully');
-});
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
+    cluster.fork();
+  });
+
+} else {
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end(`Handled by worker ${process.pid}\n`);
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Worker ${process.pid} started, listening on port ${PORT}`);
+  });
+}
