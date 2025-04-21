da3c18756dae2a9eaf03ea458763ebde18718c86
RainbowPlayer <kolak375@gmail.com>
2025-04-21 14:54:06 +0300

    add files

--- index.js
+++ index.js
@@ -7,46 +7,52 @@ const dbName = 'school';
   const client = new MongoClient(uri);
   try {
     await client.connect();
-    console.log('Connected to MongoDB');
     const db = client.db(dbName);
     const students = db.collection('students');
 
     await students.deleteMany({});
 
     const docs = [
-      { name: 'Ivan',   age: 21, group: 'A-31', marks: [75, 90, 82] },
-      { name: 'Oksana', age: 22, group: 'B-12', marks: [88, 76, 91] },
-      { name: 'Petro',  age: 20, group: 'A-31', marks: [65, 70, 72] },
-      { name: 'Maria',  age: 23, group: 'C-05', marks: [95, 98, 99] },
-      { name: 'Andriy', age: 19, group: 'B-12', marks: [80, 85, 88] }
+      { name: 'Alex',   age: 24, group: 'A-31', marks: [88, 92, 81] },
+      { name: 'Anna',   age: 22, group: 'B-12', marks: [75, 65, 70] },
+      { name: 'Bohdan', age: 19, group: 'A-31', marks: [80, 60, 55] },
+      { name: 'Alina',  age: 23, group: 'C-05', marks: [95, 90, 85] },
+      { name: 'Dmytro', age: 20, group: 'B-12', marks: [70, 72, 74] }
     ];
     await students.insertMany(docs);
-    console.log('Inserted students.');
 
     console.log('\nAll students:');
     console.table(await students.find().toArray());
 
-
-    const olderThan20 = await students.find({ age: { $gt: 20 } }).toArray();
     console.log('\nStudents older than 20:');
-    console.table(olderThan20);
+    console.table(await students.find({ age: { $gt: 20 } }).toArray());
 
-    const marksAbove85 = await students.find({ marks: { $elemMatch: { $gt: 85 } } }).toArray();
     console.log('\nStudents with marks above 85:');
-    console.table(marksAbove85);
+    console.table(await students.find({ marks: { $elemMatch: { $gt: 85 } } }).toArray());
 
-    const nameStartsA = await students.find({ name: { $regex: '^A' } }).toArray();
     console.log("\nStudents whose name starts with 'A':");
-    console.table(nameStartsA);
+    console.table(await students.find({ name: { $regex: '^A' } }).toArray());
 
-    const sortedByAgeDesc = await students.find().sort({ age: -1 }).toArray();
     console.log('\nStudents sorted by age descending:');
-    console.table(sortedByAgeDesc);
+    console.table(await students.find().sort({ age: -1 }).toArray());
 
-  } catch (err) {
-    console.error('Error:', err);
+    console.log('\nAverage mark per student:');
+    console.table(await students.aggregate([
+      { $project: { name: 1, avgMark: { $avg: '$marks' } } }
+    ]).toArray());
+
+    console.log('\nStudent count per group:');
+    console.table(await students.aggregate([
+      { $group: { _id: '$group', count: { $sum: 1 } } }
+    ]).toArray());
+
+    const overall = await students.aggregate([
+      { $unwind: '$marks' },
+      { $group: { _id: null, overallAvg: { $avg: '$marks' } } }
+    ]).toArray();
+    console.log('\nOverall average mark:', overall[0].overallAvg);
+
   } finally {
     await client.close();
-    console.log('Connection closed');
   }
 })();
