16e9dab9cf553834070b06883ab05c787446321d
RainbowPlayer <kolak375@gmail.com>
2025-05-12 21:43:45 +0300

    update code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:27017';
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const dbName = 'school';
 
 (async () => {
@@ -26,6 +26,13 @@ const dbName = 'school';
     console.log('\nAll students:');
     console.table(await students.find().toArray());
 
+    await students.updateOne({ name: 'Ivan' }, { $set: { age: 22 } });
+    console.log("\nAfter updating Ivan's age:");
+    console.table([await students.findOne({ name: 'Ivan' })]);
+
+    await students.deleteOne({ group: 'A-31' });
+    console.log('\nAfter deleting one from A-31:');
+    console.table(await students.find().toArray());
 
     const olderThan20 = await students.find({ age: { $gt: 20 } }).toArray();
     console.log('\nStudents older than 20:');
@@ -42,6 +49,26 @@ const dbName = 'school';
     const sortedByAgeDesc = await students.find().sort({ age: -1 }).toArray();
     console.log('\nStudents sorted by age descending:');
     console.table(sortedByAgeDesc);
+
+    const avgPerStudent = await students.aggregate([
+      { $project: { _id: 0, name: 1, avgMark: { $avg: '$marks' } } }
+    ]).toArray();
+    console.log('\nAverage mark per student:');
+    console.table(avgPerStudent);
+
+    const countPerGroup = await students.aggregate([
+      { $group: { _id: '$group', count: { $sum: 1 } } }
+    ]).toArray();
+    console.log('\nCount of students per group:');
+    console.table(countPerGroup);
+
+    const overallAvg = await students.aggregate([
+      { $unwind: '$marks' },
+      { $group: { _id: null, overallAvg: { $avg: '$marks' } } },
+      { $project: { _id: 0, overallAvg: 1 } }
+    ]).toArray();
+    console.log('\nOverall average mark:');
+    console.table(overallAvg);
 
   } catch (err) {
     console.error('Error:', err);
